8f046fb569896d0ab850fd781e2c7537faafd611
Mattis Wacker <mattis.wacker@gmx.de>
2021-10-25 11:47:49 +0200

    Implemented OptionCall

--- main-app/src/index.ts
+++ main-app/src/index.ts
@@ -1,4 +1,3 @@
-
 import axios from 'axios';
 import { resolve } from 'path';
 import { PluginLoader } from 'plugin-framework/out';
@@ -8,7 +7,13 @@ const pluginLoader = new PluginLoader('1.0.0');
 function start() {
     pluginLoader.loadFrom(resolve(__dirname, '../plugins')).then(async () => {
         const context = await pluginLoader.activatePlugins({ axios });
-        pluginLoader.fire('system', 'start', undefined, undefined);
+        pluginLoader.fireHook('system', 'start', undefined, undefined);
+
+        console.log(pluginLoader.options);
+        const p = pluginLoader.options[0];
+        if (p) {
+            pluginLoader.fireOptionChanged(p.pluginName, p.key, "Das ist voll der test");
+        }
     });
 }
 
--- plugins/plugin-a/src/extension.ts
+++ plugins/plugin-a/src/extension.ts
@@ -1,10 +1,16 @@
 import { PluginContext } from 'plugin-framework/out/plugin';
+import { OptionChangeEvent } from 'plugin-framework/out/plugin/option';
 
 export function activate(context: PluginContext) {
-    const name = "PLUGIN_A";
+    const name = 'PLUGIN_A';
     context.addHook<void>('system', 'start', async (sender, e) => {
         console.log(`Hallo Welt von "${name}"`);
         const response = await context.axios('https://example.com/');
         console.log(response.status);
     });
+
+    const option = context.addOption<string>('importantSetting', '', '', '');
+    option.onchange = (ev) => {
+        console.log('Settings were changed.', ev);
+    };
 }
